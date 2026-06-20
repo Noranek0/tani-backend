4b58a06e70c58f42d7d7b0fadae02d2f6cae6d86
nothing628 <yogyphang@gmail.com>
2026-06-20 21:15:06 +0700

    add cli parser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,37 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::path::PathBuf;
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Optional name to operate on
+    name: Option<String>,
+
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+    /// start serve application
+    Serve {},
+}
+
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -14,8 +47,7 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn start_http_server() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
@@ -25,4 +57,26 @@ async fn main() -> std::io::Result<()> {
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Test { list }) => {
+            if *list {
+                println!("Printing testing lists...");
+            } else {
+                println!("Not printing testing lists...");
+            }
+        }
+        Some(Commands::Serve {  }) => {
+            println!("Start listening http request...");
+            start_http_server().await?
+        }
+        None => {}
+    }
+
+    Ok(())
 }
